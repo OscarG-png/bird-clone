baa5f3b6fdb8bc71b664b0766198e832989a23c0
OscarG <oscar.gamez88@gmail.com>
2024-07-11 14:36:17 -0700

    fixed issue with submit post function

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,20 +6,23 @@ import {
   CardTitle,
 } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
+import SubmitPost from "~/server/actions/actions";
 
 export default function PostCard() {
   return (
     <Card className="rounded shadow-md">
-      <CardHeader>
-        <CardTitle>What&apos;s on your mind?</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <textarea className="rounded border p-2" />
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-        <Button>Post</Button>
-      </CardFooter>
+      <form action={SubmitPost}>
+        <CardHeader>
+          <CardTitle>What&apos;s on your mind?</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <textarea className="rounded border p-2" required />
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+          <Button type="submit">Post</Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
---
+++ src/server/actions/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export default async function SubmitPost(): Promise<string> {
+  return "success";
+}
