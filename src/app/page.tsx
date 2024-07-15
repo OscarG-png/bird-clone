b4e733d6053451b06a8ddc57060e217f2f6036fa
OscarG <oscar.gamez88@gmail.com>
2024-07-15 13:03:00 -0700

    testing get posts functions on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import PostCard from "~/components/PostCard";
+import { getPosts } from "~/server/actions/actions";
 
 export default function HomePage() {
+  const posts = getPosts();
+  console.log("Posts: ", posts);
   return (
     <main className="flex min-h-screen flex-col items-center">
       <PostCard />
