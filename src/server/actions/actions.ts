b78738d6cf415bc004bf9a571f892be4fd91db6c
OscarG <oscar.gamez88@gmail.com>
2024-07-15 10:46:37 -0700

    completed server actions

--- src/server/actions/actions.ts
+++ src/server/actions/actions.ts
@@ -1,6 +1,26 @@
-"use server";
+import "server-only";
+import { db } from "~/server/db";
 
-export default async function SubmitPost(): Promise<string> {
+export async function SubmitPost(): Promise<string> {
   console.log("Post submitted!");
   return "success";
+}
+export async function getPosts() {
+  const posts = await db.query.posts.findMany({
+    orderBy: (model, { desc }) => desc(model.createdAt),
+  });
+  if (!posts) {
+    throw new Error("No posts found");
+  }
+  return posts;
+}
+
+export async function getPostById(id: number) {
+  const post = await db.query.posts.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
+  if (!post) {
+    throw new Error("Post not found");
+  }
+  return post;
 }
