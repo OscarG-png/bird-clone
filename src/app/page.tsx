9d5b49a43bcbdd18baa76cdbedcdd7cb08256fdd
OscarG <oscar.gamez88@gmail.com>
2024-07-23 13:35:37 -0700

    left myself a note for tomorrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import PostCard from "~/components/PostCard";
 import { getPosts } from "~/server/actions/actions";
 import { Separator } from "~/components/ui/separator";
-import { auth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 export default function HomePage() {
   const user = auth();
+  console.log("user: ", user);
 
-  console.log(user);
   return (
     <main className="flex min-h-screen flex-col items-center gap-2">
       <PostCard />
@@ -17,14 +17,14 @@ export default function HomePage() {
 
 async function Posts() {
   const posts = await getPosts();
+  const curruser = await currentUser();
+  console.log("curruser: ", curruser);
+  // this version gives me access to things like username and user image url
   return (
     <>
       {posts.map((post) => (
         <div key={post.id}>
-          <div
-            key={post.id}
-            className="flex w-72 flex-col items-center rounded border p-2"
-          >
+          <div className="flex w-72 flex-col items-center rounded border p-2">
             <div className="flex w-full justify-between">
               <h1>{post.user}</h1>
               <p className="text-sm italic">{post.createdAt.toDateString()}</p>
