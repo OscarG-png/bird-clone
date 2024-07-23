ea8c7b60df18692703fe535fac10d36d4ec7497b
OscarG <oscar.gamez88@gmail.com>
2024-07-23 12:47:04 -0700

    changed fragment to div, was causing key warnigns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import PostCard from "~/components/PostCard";
 import { getPosts } from "~/server/actions/actions";
 import { Separator } from "~/components/ui/separator";
+import { auth } from "@clerk/nextjs/server";
 
 export default function HomePage() {
+  const user = auth();
+
+  console.log(user);
   return (
     <main className="flex min-h-screen flex-col items-center gap-2">
       <PostCard />
@@ -16,7 +20,7 @@ async function Posts() {
   return (
     <>
       {posts.map((post) => (
-        <>
+        <div key={post.id}>
           <div
             key={post.id}
             className="flex w-72 flex-col items-center rounded border p-2"
@@ -28,7 +32,7 @@ async function Posts() {
             <p>{post.content}</p>
           </div>
           <Separator className="" />
-        </>
+        </div>
       ))}
     </>
   );
