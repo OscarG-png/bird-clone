f080b41efd560b13d74deb84c69bbc3e3bdf88d3
OscarG <oscar.gamez88@gmail.com>
2024-08-15 12:29:58 -0700

    added user images to each post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import PostCard from "~/components/PostCard";
 import { getPosts } from "~/server/actions/queries";
 import { Separator } from "~/components/ui/separator";
+import Image from "next/image";
 
 export default function HomePage() {
   // const user = auth();
@@ -23,11 +24,20 @@ async function Posts() {
         <div key={post.id}>
           <div className="flex w-72 flex-col items-center rounded border p-2 shadow-md">
             <div className="flex w-full justify-between">
-              <h1>{post.user}</h1>
+              <div className="flex flex-row">
+                <Image
+                  src={`${post.userImage}`}
+                  alt="user image"
+                  width={50}
+                  height={50}
+                  className="rounded-full"
+                />
+                <h1>{post.user}</h1>
+              </div>
               <p className="text-sm italic">{post.createdAt.toDateString()}</p>
             </div>
             <p>{post.content}</p>
-            <div>
+            <div className="flex flex-row gap-2">
               {post.tags.map((tag) => (
                 <span key={tag.id} className="text-sm italic">
                   {tag.tag}
