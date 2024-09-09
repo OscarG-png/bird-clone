dd72c92f64c1e87defc8a58436b9ae78523a9f81
OscarG <oscar.gamez88@gmail.com>
2024-09-09 11:59:13 -0700

    small styling on post detail page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,16 +6,16 @@ export default async function PostPage({ params }: { params: { id: string } }) {
   const post = await getPostById(parseInt(id));
   console.log(post);
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-2">
       <h1>PostPage</h1>
-      <div>
-        <div id="user" className="flex flex-row items-center gap-2">
+      <div className="rounded-xl border-2 p-5">
+        <div id="user" className="flex flex-row items-center gap-6">
           <Image
             src={post.userImage}
             className="rounded-full"
             alt={post.user}
-            width={50}
-            height={50}
+            width={60}
+            height={60}
           />
           <h2>{post.user}</h2>
           <p>{post.createdAt.toDateString()}</p>
